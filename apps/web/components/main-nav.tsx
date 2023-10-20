df399bd1b742a2fd7329b0e3fd3b63b23a375d7b
Pawan Paudel <pawanpaudel93@gmail.com>
2023-10-20 16:52:50 +0545

    fix: Add hover effect on nav items

--- apps/web/components/main-nav.tsx
+++ apps/web/components/main-nav.tsx
@@ -32,7 +32,7 @@ export function MainNav({ items, children }: MainNavProps) {
                   key={index}
                   href={item.href}
                   className={cn(
-                    "flex items-center text-sm font-medium text-muted-foreground",
+                    "flex items-center text-lg font-medium transition-colors hover:text-foreground/80 sm:text-sm text-foreground/60",
                     item.disabled && "cursor-not-allowed opacity-80"
                   )}
                 >
