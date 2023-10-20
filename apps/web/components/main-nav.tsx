13620094a9c420faee8e6a591003490791ce754c
Pawan Paudel <pawanpaudel93@gmail.com>
2023-10-20 16:43:18 +0545

    feat: Add mobile navigation

--- apps/web/components/main-nav.tsx
+++ apps/web/components/main-nav.tsx
@@ -5,20 +5,26 @@ import { NavItem } from "@/types/nav";
 import { siteConfig } from "@/config/site";
 import { cn } from "@/lib/utils";
 import { Icons } from "@/components/icons";
+import { MobileNav } from "./mobile-nav";
 
 interface MainNavProps {
   items?: NavItem[];
+  children?: React.ReactNode;
 }
 
-export function MainNav({ items }: MainNavProps) {
+export function MainNav({ items, children }: MainNavProps) {
+  const [showMobileMenu, setShowMobileMenu] = React.useState<boolean>(false);
+
   return (
     <div className="flex gap-6 md:gap-10">
-      <Link href="/" className="flex items-center space-x-2">
+      <Link href="/" className="hidden md:flex items-center space-x-2">
         <Icons.logo className="h-6 w-6" />
-        <span className="inline-block font-bold">{siteConfig.name}</span>
+        <span className="hidden sm:inline-block font-bold">
+          {siteConfig.name}
+        </span>
       </Link>
       {items?.length ? (
-        <nav className="flex gap-6">
+        <nav className="hidden md:flex gap-6">
           {items?.map(
             (item, index) =>
               item.href && (
@@ -36,6 +42,16 @@ export function MainNav({ items }: MainNavProps) {
           )}
         </nav>
       ) : null}
+      <button
+        className="flex items-center space-x-2 md:hidden"
+        onClick={() => setShowMobileMenu(!showMobileMenu)}
+      >
+        {showMobileMenu ? <Icons.close /> : <Icons.logo />}
+        <span className="font-bold">Menu</span>
+      </button>
+      {showMobileMenu && items && (
+        <MobileNav items={items}>{children}</MobileNav>
+      )}
     </div>
   );
 }
