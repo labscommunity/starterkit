cfba09e08314b38456c7fb6c626e9928a3d6abcb
Pawan Paudel <pawanpaudel93@gmail.com>
2023-10-23 23:01:32 +0545

    refactor: Update cli name to create-arweave-app

--- cli/src/utils/renderVersionWarning.ts
+++ cli/src/utils/renderVersionWarning.ts
@@ -8,13 +8,13 @@ export const renderVersionWarning = (npmVersion: string) => {
   const currentVersion = getVersion();
 
   if (currentVersion.includes("beta")) {
-    logger.warn("  You are using a beta version of create-perma-app.");
+    logger.warn("  You are using a beta version of create-arweave-app.");
     logger.warn("  Please report any bugs you encounter.");
   } else if (currentVersion.includes("next")) {
-    logger.warn("  You are running create-perma-app with the @next tag which is no longer maintained.");
+    logger.warn("  You are running create-arweave-app with the @next tag which is no longer maintained.");
     logger.warn("  Please run the CLI with @latest instead.");
   } else if (currentVersion !== npmVersion) {
-    logger.warn("  You are using an outdated version of create-perma-app.");
+    logger.warn("  You are using an outdated version of create-arweave-app.");
     logger.warn("  Your version:", currentVersion + ".", "Latest version in the npm registry:", npmVersion);
     logger.warn("  Please run the CLI with @latest to get the latest updates.");
   }
@@ -35,7 +35,7 @@ interface DistTagsBody {
 function checkForLatestVersion(): Promise<string> {
   return new Promise((resolve, reject) => {
     https
-      .get("https://registry.npmjs.org/-/package/create-perma-app/dist-tags", (res) => {
+      .get("https://registry.npmjs.org/-/package/create-arweave-app/dist-tags", (res) => {
         if (res.statusCode === 200) {
           let body = "";
           res.on("data", (data) => (body += data));
@@ -57,7 +57,7 @@ export const getNpmVersion = () =>
   // `fetch` to the registry is faster than `npm view` so we try that first
   checkForLatestVersion().catch(() => {
     try {
-      return execSync("npm view create-perma-app version").toString().trim();
+      return execSync("npm view create-arweave-app version").toString().trim();
     } catch {
       return null;
     }
